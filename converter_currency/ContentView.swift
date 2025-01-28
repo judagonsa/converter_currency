9a979d4cbae25b31c4611691b6ec95b16e979d46
judagonsa <judagonsa8@gmail.com>
2025-01-28 12:30:26 -0500

    git init, create project

---
+++ converter_currency/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  converter_currency
+//
+//  Created by Julian González on 28/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ converter_currency/converter_currencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  converter_currencyApp.swift
+//  converter_currency
+//
+//  Created by Julian González on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct converter_currencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
